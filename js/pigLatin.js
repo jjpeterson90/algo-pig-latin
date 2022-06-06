0fb41016eeb01013950070c0a8f8a928fd04b042
Justin <jjpeterson90@hotmail.com>
2022-06-06 15:32:12 -0500

    imperfect completion

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,50 @@
-exports.translate = function(word) {
+exports.translate = function(str) {
+
+    let vowels = "aeiou'";
+    let sentences = [];
+    // pushes each new line into its own array element
+    sentences.push(...str.split("\n^'"));
+    //console.log(sentences)
+
+    // iterate through each phrase in the array
+    for (let phrase = 0; phrase < sentences.length; phrase++) {
+        sentences[phrase] = sentences[phrase].split(/\b/);  // splits words and non-words into individual elements
+        for (let word = 0; word < sentences[phrase].length; word++) { // iterates through each element in this phrase
+            if (/[a-zA-z]/.test(sentences[phrase][word])) { // only do this on elements with words
+                if (sentences[phrase][word+1] !== "'") {
+                    sentences[phrase][word] = translateWord(sentences[phrase][word]);
+                }
+            }
+        }
+        sentences[phrase] = sentences[phrase].join('')
+    }
+    sentences = sentences.join("\n");
+
+    function translateWord(word) {
+        word = word.split('');
+        if (vowels.includes(word[0])) {
+            word.push("ay");
+        // } else if () {
+        } else {
+            let vowelMatch = word.join('').match(/[aeio]/g) || 0;
+            let vowelIndex = word.indexOf(vowelMatch[0])
+            for (let i = 0; i < vowelIndex; i++) {
+                if (word[0] === word[0].toUpperCase()) {
+                    word[0] = word[0].toLowerCase();
+                    word[vowelIndex] = word[vowelIndex].toUpperCase();
+                }
+                word.push(word.shift())
+            }
+            word.push("ay");
+        }
+        word = word.join('');
+        return word
+    }
+
+    return sentences
+
+
 };
+
+
+// "The quick, brown fox!") === "Ethay ickquay, ownbray oxfay!"
--- js/pigLatinSpec.js
+++ js/pigLatinSpec.js
@@ -21,6 +21,21 @@ console.log(pig.translate("square") === "aresquay");
 console.log("translates many words:");
 console.log(pig.translate("the quick brown fox") === "ethay ickquay ownbray oxfay");
 
+// console.log(`${pig.translate("apple")} === appleay`);
+// console.log(`${pig.translate("banana")} === ananabay`);
+// console.log(`${pig.translate("cherry")} === errychay`);
+// console.log(`${pig.translate("eat pie")} === eatay iepay`);
+// console.log(`${pig.translate("three")} === eethray`);
+// console.log(`${pig.translate("school")} === oolschay`);
+// console.log(`${pig.translate("quiet")} === ietquay`);
+// console.log(`${pig.translate("square")} === aresquay`);
+// console.log()
+// console.log(`${pig.translate("university")} === universityay`)
+// console.log(`${pig.translate("apple university")} === appleay universityay`)
+// console.log(`${pig.translate("School")} === Oolschay`)
+// console.log(`${pig.translate("I go to apple university")} === Iay ogay otay appleay universityay.`)
+// console.log(`${pig.translate("Hi, I'm Zach.\nHow are you?")}\n=== Ihay, I'may Achzay.\n=== Owhay areay ouyay?`)
+
 // write a test asserting that capitalized words are still capitalized
 // (but with a different initial capital letter, of course) retain the
 // punctuation from the original phrase
